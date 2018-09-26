e49f0743f93a6d0b924a41c64bbbcc935789d155
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-09-26 11:07:57 -0400

    edit batch script

--- chr_dbsnp.bat
+++ chr_dbsnp.bat
@@ -1,1 +1,7 @@
+#!/bin/sh
+
 python chrjson_dbSNP.py
+
+echo "Completed chrjson_dbSNP.py, now sleeping for 2 mins"
+
+sleep 120
--- rs_dbsnp.bat
+++ rs_dbsnp.bat
@@ -1,1 +1,7 @@
+#!/bin/sh
+
 python rsjson_dbSNP.py
+
+echo "Completed rsjson_dbSNP.py, now sleeping for 2 mins"
+
+sleep 120
